e4bd3e0e04dffcff31e5202755284ec2a48e8497
manav15dhupar-ux <manav15dhupar@gmail.com>
2026-01-07 07:33:03 -0800

    Add File Management System

--- Project_02/main.py
+++ Project_02/main.py
@@ -1,6 +1,20 @@
+def save_data(data,date,name):
+    with open("sales.txt","a") as f:
+        f.write(f"Rs.{str(data)},{str(date)},{str(name)}\n")
+
+def get_date():
+    date_pur=input("Enter The Date(DD/MM/YY):")
+    return date_pur
+
+def costomer_details():
+    name=input("Enter Name:")
+    return name
+
 print("======= Welcome To krina Store Calculator =======")
 add=0
 amt_list=[]
+today_date=get_date()
+custom=costomer_details()
 
 while True:
     num=input("\nEnter the amount or press q to exit:\n")
@@ -10,7 +24,8 @@ while True:
         total=float(num)
         add=add+total
         print(f"Total so far {add}")
-        amt_list.append(float(num))
+        amt_list.append(total)
+
     else:
         print("Enter a valid amount:")
 
@@ -18,4 +33,5 @@ print("\n======MANAV GENERAL STORE======\n")
 for i in amt_list:
     print(f"Rs.{i}")
 print(f"Total amount Rs.{add} , Thanks for coming 😁 Visit Again 🙏")
+save_data(add,today_date,custom)
 
