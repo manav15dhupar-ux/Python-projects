18bb144bca8d478aed72271e7ac9d57773f21b10
manav15dhupar-ux <manav15dhupar@gmail.com>
2026-01-08 06:11:30 -0800

    correct logic

--- Project_04/main.py
+++ Project_04/main.py
@@ -1,30 +1,28 @@
-def load_data():
-    try
+#------Loads Previous data------
 
-print("====== WELCOME TO PHONE DIRECTORY ======")
+def load_data():
+    try:
+        with open("ph.txt","r") as f:
+            for line in f:
+                name,ph=line.strip().split(',')
+                phone[name]=ph
+
+    except FileNotFoundError:
+        pass
+
+#------Add New Data To File------
+
+def save_data():
+    with open("ph.txt","w") as f:
+        for name,ph in phone.items():
+            f.write(f"{str(name)},{str(ph)}\n")
+
+#------Main Menu------
 
 phone = {}
-n = int(input("Enter the Number Of Entries: "))
+load_data() # Fetching Previous Data
 
-for i in range(n):
-    name = input("Enter Name: ")
-    if name in phone:
-        print("The name already exist")
-        continue
-    
-       
-    ph = input("Enter Phone Number: ")
-    if ph in phone.values():
-        print("phone number already exist.")
-        continue
-   
-       
-
-    if ph.isdigit() and len(ph)==10:
-        phone[name] = ph
-    else:
-        print("Enter a valid number, start again")
-        break
+print("====== WELCOME TO PHONE DIRECTORY ======")
 
 while True:
     print("\n====== SEARCH LIST ======")
@@ -37,43 +35,59 @@ while True:
     ch = int(input("Enter your choice: "))
 
     if ch == 1:
+        found=False
         for key in phone:
             print(key)
+            found=True
+        if not found:
+            print("No contact list exist")
 
     elif ch == 2:
-        find = input("Enter the name to find contact: ")
-        if find in phone:
-            print(find, ":", phone[find])
+        if(len(phone)!=0):
+            find = input("Enter the name to find contact: ")
+            if find in phone:
+                print(f"{find}:{phone[find]}")
+            else:
+                print("Contact not found. Choose option 3 to add.")
+        
         else:
-            print("Contact not found. Choose option 3 to add.")
+            print("No contacts exist")
 
     elif ch == 3:
-        name_=input("Enter the name:")
-        ph_=input("Enter the phone number:")
+        n = int(input("Enter the Number Of Entries: "))
 
-        if name_ in phone:
-            print("The name already exist")
-            continue
-  
+        for i in range(n):
+            name = input("Enter Name: ")
+            if name in phone:
+                print("The name already exist")
+                continue
     
-        if ph_ in phone.values():
-            print("phone number already exist.")
-            continue
+            ph = input("Enter Phone Number: ")
+            if ph in phone.values():
+                print("phone number already exist.")
+                continue
+   
+    
 
-
-        if ph_.isdigit() and len(ph_)==10:
-            phone[name_] = ph_
-            print("Contact added successfully.")
-        else:
-            print("Enter a valid phone number")
+            if ph.isdigit() and len(ph)==10:
+                phone[name] = ph
+                save_data() # Stores New Data
+            else:
+                print("Enter a valid number, start again")
+                break
 
     elif ch == 4:
-        name_dis = input("Enter name of contact to discard: ")
-        if name_dis in phone:
-            phone.pop(name_dis)
-            print("Contact removed successfully.")
+        if(len(phone)!=0):
+            name_dis = input("Enter name of contact to discard: ")
+            if name_dis in phone:
+                phone.pop(name_dis)
+                save_data() # Removing Data From File
+                print("Contact removed successfully.")
+            else:
+                print("The contact does not exist.")
+        
         else:
-            print("The contact does not exist.")
+            print("Contact list does not exist")
 
     elif ch == 5:
         break
